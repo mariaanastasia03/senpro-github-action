f35f66604f3aca6cb207979fd683dfbd7b22ba1a
mariaanastasia03 <mariatambunan03@gmail.com>
2023-03-17 19:15:04 +0700

    case2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-// import Head from 'next/head'
-// import Image from 'next/image'
-// import { Inter } from '@next/font/google'
-// import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '@/styles/Home.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -32,7 +32,7 @@ export default function Home() {
         </div>
 
         <div>
-          <h5 className={inter.className}>[Maria Anastasia] | [460551]</h5>
+          <h5 className={inter.className}>[Nama] | [NIU]</h5>
         </div>
       </main>
     </>
